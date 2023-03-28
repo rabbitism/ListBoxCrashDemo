968710763680d735d616687e577cc8a59c69acd3
rabbitism <popmessiah@hotmail.com>
2023-03-28 23:10:38 +0800

    feat: add demo

---
+++ AvaloniaApplication1/Views/MainWindow.axaml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Drawing;
+using Avalonia.Input;
+using Avalonia.Layout;
+using Avalonia.Styling;
+using Avalonia.Threading;
+
+namespace AvaloniaApplication1.Views
+{
+    public partial class MainWindow : Window, IStyleable
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
